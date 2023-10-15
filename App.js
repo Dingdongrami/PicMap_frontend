a432113a69fd694b22c8fc04030b332a9338ae3f
Kong-E <kng001016@dgu.ac.kr>
2023-10-15 16:27:45 +0900

    feat: 커스텀 폰트 추가

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
+import React, { useEffect, useState } from 'react'; // useState 추가
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    IropkeBatang: require('./assets/fonts/IropkeBatangM.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>테스트테스트테스트테스트테스트테스트테스트테스트</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'IropkeBatang',
+    fontSize: 15,
   },
 });
