3913af493e2fa76773a865b8be26a3e7035c205f
Kong-E <kng001016@dgu.ac.kr>
2023-10-15 15:05:52 +0900

    chore: 색상 변수 지정 및 prettierrc 설정

---
+++ constants/colors.js
@@ -0,0 +1,20 @@
+const COLORS = {
+  beige: '#FCFCF2',
+  brown1: '#F5F5F4',
+  brown2: '#E7E5E4',
+  brown3: '#D6D3D1',
+  brown4: '#A8A29E',
+  brown5: '#78716C',
+  brown6: '#57534E',
+  brown7: '#44403C',
+  pink1: '#FFECEA',
+  pink2: '#FFEE30',
+  pink3: '#FFB3BA',
+  pink4: '#EB9090',
+  red: '#E53A40',
+  pink1_80: 'rgba(255, 236, 234, 0.8)',
+  pink2_40: 'rgba(255, 238, 48, 0.4)',
+  brown2_80: 'rgba(231, 229, 228, 0.8)',
+};
+
+export default COLORS;
