f6f049660832676e20e925c00dad1f49998378da
Kong-E <kng001016@dgu.ac.kr>
2023-10-16 16:23:54 +0900

    feat: MyPage Post 탭 UI 구현

--- screens/MyPage.js
+++ screens/MyPage.js
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
+import { CirclePost } from '../components';
 
 export const MyPage = () => {
+  // Example data (let's say you want to render 10 CirclePost components)
+  const [data, setData] = useState(Array(10).fill({}));
+
   return (
-    <View>
-      <Text>MyPage</Text>
-    </View>
+    <FlatList
+      data={data}
+      renderItem={({ item, index }) => <CirclePost />}
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={{ padding: 16 }} // Optional: for some padding around your list
+    />
   );
 };
