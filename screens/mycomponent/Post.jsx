5e2fc15a31ea96cb8bdd85f902e33dbc5f10bddc
g2Min <jimin9320@gmail.com>
2023-10-22 01:38:08 +0900

    feat: post, circle 기본 화면 구현

---
+++ screens/mycomponent/Post.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export const Post = () => {
+  return(
+    <View style={styles.container}>
+      <Text>Post</Text>
+    </View>
+  )
+}
