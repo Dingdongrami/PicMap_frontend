201b4f3d0d41bfedc8d529982726d78b9e17eeff
g2Min <jimin9320@gmail.com>
2023-10-22 01:38:34 +0900

    feat: map API 연결 설정

---
+++ screens/mycomponent/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%"
+  }
+})
