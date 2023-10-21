6bd0baa53fa6fda7b49d9f4945ec073f8003a880
g2Min <jimin9320@gmail.com>
2023-10-22 01:37:52 +0900

    feat: post | map | circle 탭 구현

--- components/header/Stack/AltScreen.jsx
+++ components/header/Stack/AltScreen.jsx
@@ -1,13 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Search, TimeLine, MyPage } from "../../../screens";
+import { Search, TimeLine } from "../../../screens";
 import { Header } from "../Header";
+import { AltComponent } from '../../middle/Stack/AltComponent';
 
 const Stack = createNativeStackNavigator();
 
 export const AltScreen = () => {
-  const right = () => {
-    console.log("right?");
-  }
   return(
     <Stack.Navigator 
       initialRouteName="MyPage"
@@ -17,7 +15,7 @@ export const AltScreen = () => {
     >
       <Stack.Screen name="Search" component={Search} />
       <Stack.Screen name="TimeLine" component={TimeLine} />
-      <Stack.Screen name="MyPage" component={MyPage} />
+      <Stack.Screen name="MyPage" component={AltComponent} />
     </Stack.Navigator>
   )
 }
