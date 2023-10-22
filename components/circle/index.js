413f8ce6e071f4eb0d81608aaf34e95d87fdff0e
g2Min <jimin9320@gmail.com>
2023-10-23 02:14:18 +0900

    chore: 경로 재정리 및 불필요한 import 제거

--- components/circle/index.js
+++
@@ -1,1 +0,0 @@
-export { CircleRoom } from './CircleRoom';
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,5 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
-import { TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { styles } from "./styles";
 
