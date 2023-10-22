a1f992243127d2cd7852dc20adddeb39438be447
g2Min <jimin9320@gmail.com>
2023-10-22 16:34:48 +0900

    feat: 써클 rendering 구현

--- screens/mycomponent/Circle.jsx
+++ screens/mycomponent/Circle.jsx
@@ -1,10 +1,33 @@
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, SafeAreaView,  } from 'react-native';
 import { styles } from './styles';
+import { CircleRoom } from '../../components/circle';
+import { useState } from 'react';
 
 export const Circle = () => {
+  // const [ data, setData ] = useState(Array(10).fill({}));
+  const data = [
+    { name: '써클1' },
+    { name: '써클2' },
+    { name: '써클3' },
+    { name: '써클4' },
+    { name: '써클5' },
+    { name: '써클6' },
+    { name: '써클7' },
+    { name: '써클8' },
+    { name: '써클9' },
+    { name: '써클10' },
+  ]
+  const [ loading, setLoading ] = useState(false);
+
   return(
-    <View style={styles.container}>
-      <Text>Circle</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={data}
+          renderItem={({item}) => <CircleRoom item={item} />}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={2}
+          showVerticalScrollIndicator={false}
+        />
+    </SafeAreaView>
   )
 }
