0ac8e7ab06ad3c352959f248e52a3de31a328094
g2Min <jimin9320@gmail.com>
2023-10-23 02:14:18 +0900

    chore: 경로 재정리 및 불필요한 import 제거

--- screens/mycomponent/styles.js
+++ screens/mycomponent/styles.js
@@ -3,12 +3,22 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
-    borderWidth: 1,
+    // borderWidth: 1,
+    backgroundColor: 'white',
     marginTop: 50
   },
   map: {
     width: "100%",
     height: "100%"
-  }
+  },
+  // overlay: {
+  //   position: 'absolute',
+  //   top: 507,
+  //   left: 23,
+  //   right: 312,
+  //   bottom: 16,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   flex: 1
+  // }
 })
