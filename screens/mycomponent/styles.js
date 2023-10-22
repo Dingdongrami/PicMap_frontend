fb26d3a39d72102bdc5e00102daf62004fd335cf
g2Min <jimin9320@gmail.com>
2023-10-22 16:34:48 +0900

    feat: 써클 rendering 구현

--- screens/mycomponent/styles.js
+++ screens/mycomponent/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    // alignItems: 'center',
     borderWidth: 1,
     marginTop: 50
   },
