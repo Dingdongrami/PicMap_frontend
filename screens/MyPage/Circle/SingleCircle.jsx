e87cd4d82c6e19885e3022efef4cc2edc7e00afb
g2Min <jimin9320@gmail.com>
2023-11-06 05:17:52 +0900

    feat: 다른사람 프로필 UI

--- screens/MyPage/Circle/SingleCircle.jsx
+++ screens/MyPage/Circle/SingleCircle.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, FlatList, Button, Image  } from 'react-native';
+import { View, Text, Pressable, FlatList, Button, Image, Animated, PanResponder  } from 'react-native';
 import { useState } from 'react';
 import { SplashUI } from './SplashUI';
 import { splashState } from '../../../stores/splash-store';
@@ -7,6 +7,7 @@ import { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import MapView from 'react-native-maps';
 import { SingleMap } from '../../../components/circle/single/SingleMap';
 import { SinglePhotoIcon } from '../../../components/circle/album/SinglePhotoIcon';
+import { OthersProfile } from '../../../components/MyProfile/OthersProfile';
 
 export const SingleCircle = ({ route }) => {
   const [ isReady, setIsReady ] = useState(splashState);
@@ -19,59 +20,44 @@ export const SingleCircle = ({ route }) => {
   } 
   else{
     return(
-      // <Screen
-      //   name="SingleCircle"
-      //   component={SingleCircle}
-      //   options={{
-      //     headerRight: () => (
-      //       <Image source={require('../../../assets/icons/circle_array_btn.png')} style={styles.rightHeader} >
-      //         <Button 
-      //           title="Custom Button"
-      //           onPress={() => {
-      //           }}
-      //         />
-      //       </Image>
-
-      //     )
-      //   }}
-      // >
-        <View style={{flex:1, flexDirection: 'column', backgroundColor: '#fff'}}>
-          <View style={styles.personBox} />
-          {/* <SingleMap /> */}
-          <MapView
-            style={styles.mapContainer}
-            initialRegion={{
+      <View style={{flex:1, flexDirection: 'column', backgroundColor: '#fff'}}>
+        <View style={styles.personBox}> 
+          <OthersProfile />
+        </View>
+        {/* <SingleMap /> */}
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={{
+            latitude: 37.580112,
+            longitude: 126.977166,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+          provider={PROVIDER_GOOGLE}>
+          <Marker
+            coordinate={{
               latitude: 37.580112,
               longitude: 126.977166,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01,
             }}
-            provider={PROVIDER_GOOGLE}>
-            <Marker
-              coordinate={{
-                latitude: 37.580112,
-                longitude: 126.977166,
-              }}
-            />
-          </MapView>
-          <View style={styles.wrapper} >
-            <Text style={styles.imageText}>사진</Text>
-            <Pressable style={styles.optionButton}>
-              <Text style={styles.optionText}>선택</Text>
-            </Pressable>
-          </View>
-          <View style={styles.albumContainer}>
-            <FlatList 
-              data={album}
-              renderItem={({ item }) => <SinglePhotoIcon item={item} />}
-              keyExtractor={(item, index) => index.toString()}
-              numColumns={3}
-              showsVerticalScrollIndicator={true}
-            />
-          </View>
-        </View>
-      // </Screen>
+          />
+        </MapView>
 
+        <View style={styles.wrapper} >
+          <Text style={styles.imageText}>사진</Text>
+          <Pressable style={styles.optionButton}>
+            <Text style={styles.optionText}>선택</Text>
+          </Pressable>
+        </View>
+        <View style={styles.albumContainer}>
+          <FlatList 
+            data={album}
+            renderItem={({ item }) => <SinglePhotoIcon item={item} />}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={3}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
     );
   }
 };
