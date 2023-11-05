d7a0aef5f5c64079f34fa5cc459b78d3adf1a1ff
g2Min <jimin9320@gmail.com>
2023-11-05 22:01:07 +0900

    feat: param값 전달 + style: 스플래시 화면 gap 수정

--- screens/MyPage/Circle/SplashUI.jsx
+++ screens/MyPage/Circle/SplashUI.jsx
@@ -1,24 +1,47 @@
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { useEffect } from 'react';
+import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
 import { useRecoilState } from 'recoil';
 import { splashState } from '../../../stores/splash-store';
 
-export const SplashUI = () => {
+export const SplashUI = ({ route }) => {
   const [ isReady, setIsReady ] = useRecoilState(splashState);
   const navigation = useNavigation();
+  const { itemId } = route.params;
   useEffect(() => {
     setTimeout(() => {
-      navigation.navigate('SingleCircle');
+      navigation.navigate('SingleCircle', { itemId });
       setIsReady(true);
-    }, 3000);
+    }, 4000);
   }, []);
+  
+  //써클 참여자 더미데이터
+  const persons = [
+    { id: 1, name: '김' },
+    { id: 2, name: '이' },
+    { id: 3, name: '박' },
+    { id: 4, name: '최' },
+  ];
+  const ListPersons = ({item}) => {
+    return(
+      <View style={styles.personCircle}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
 
   return(
-    <View>
-      <Text>
-        대기화면입니다.
-      </Text>
+    <View style={styles.splashContainer}>
+      <View style={styles.memberContainer}>
+        <FlatList 
+          data={persons}
+          renderItem={({ item }) => <ListPersons item={item} />}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 };
--- screens/MyPage/Circle/styles.js
+++ screens/MyPage/Circle/styles.js
@@ -9,4 +9,32 @@ export const styles = StyleSheet.create({
     marginTop: 40,
     paddingHorizontal: 8,
   },
+  splashContainer: {
+    flex: 1,
+    backgroundColor: 'linear-gradient(0deg, var(--pink2_40, rgba(255, 227, 224, 0.40)) 0%, var(--pink2_40, rgba(255, 227, 224, 0.40)) 100%), #FFF',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  memberContainer: {
+    borderWidth: 0.5,
+    borderColor: '#44403C',
+    width: 164,
+    height: 164,
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 20.5,
+  },
+  personCircle: {
+    marginHorizontal: 5.5,
+    marginVertical: 6,
+    borderWidth: 0.5,
+    borderColor: '#78716C',
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
