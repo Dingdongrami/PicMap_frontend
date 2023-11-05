f47341888b126ca35e9d9c73405bab79447f5b2f
g2Min <jimin9320@gmail.com>
2023-11-06 05:49:48 +0900

    feat: 스크롤 바 위치에 따른 map과 header의 display 설정

--- components/circle/album/styles.js
+++ components/circle/album/styles.js
@@ -2,11 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   imageContainer: {
+    flex: 1,
     width: 120,
     height: 120,
     borderWidth: 0.5,
     borderColor: '#44403C',
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginHorizontal: 3,
--- screens/MyPage/Circle/SingleCircle.jsx
+++ screens/MyPage/Circle/SingleCircle.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, FlatList, Button, Image, Animated, PanResponder  } from 'react-native';
+import { View, Text, Pressable, FlatList, ScrollView  } from 'react-native';
 import { useState } from 'react';
 import { SplashUI } from './SplashUI';
 import { splashState } from '../../../stores/splash-store';
@@ -11,9 +11,25 @@ import { OthersProfile } from '../../../components/MyProfile/OthersProfile';
 
 export const SingleCircle = ({ route }) => {
   const [ isReady, setIsReady ] = useState(splashState);
+  const [ isMap, setIsMap ] = useState(true);
   //써클의 id값 찾아내기
   const { itemId } = route.params;
-  const album = Array(15).fill();
+  const album = Array(90).fill();
+  const itemsPerRow = 3;
+  const groupedData = [];
+  for(let i=0; i<album.length; i+=itemsPerRow) {
+    groupedData.push(album.slice(i, i+itemsPerRow));
+  }
+
+  const handleScroll = (e) => {
+    //스크롤 위치를 확인
+    const yOffset = e.nativeEvent.contentOffset.y;
+    if(yOffset > 110){
+      setIsMap(false);
+    }else if(yOffset <=0){
+      setIsMap(true);
+    }
+  };
 
   if(!isReady) {
     return <SplashUI />
@@ -21,42 +37,45 @@ export const SingleCircle = ({ route }) => {
   else{
     return(
       <View style={{flex:1, flexDirection: 'column', backgroundColor: '#fff'}}>
-        <View style={styles.personBox}> 
-          <OthersProfile />
-        </View>
-        {/* <SingleMap /> */}
-        <MapView
-          style={styles.mapContainer}
-          initialRegion={{
-            latitude: 37.580112,
-            longitude: 126.977166,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01,
-          }}
-          provider={PROVIDER_GOOGLE}>
-          <Marker
-            coordinate={{
-              latitude: 37.580112,
-              longitude: 126.977166,
-            }}
-          />
-        </MapView>
-
-        <View style={styles.wrapper} >
-          <Text style={styles.imageText}>사진</Text>
-          <Pressable style={styles.optionButton}>
-            <Text style={styles.optionText}>선택</Text>
-          </Pressable>
-        </View>
-        <View style={styles.albumContainer}>
-          <FlatList 
-            data={album}
-            renderItem={({ item }) => <SinglePhotoIcon item={item} />}
-            keyExtractor={(item, index) => index.toString()}
-            numColumns={3}
-            showsVerticalScrollIndicator={false}
-          />
-        </View>
+        <ScrollView onScroll={handleScroll} scrollEventThrottle={16}>
+          <View style={styles.personBox}> 
+            <OthersProfile />
+          </View>
+          {/* <SingleMap /> */}
+          {isMap && (
+            <MapView
+              style={styles.mapContainer}
+              initialRegion={{
+                latitude: 37.580112,
+                longitude: 126.977166,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+              }}
+              provider={PROVIDER_GOOGLE}>
+              <Marker
+                coordinate={{
+                  latitude: 37.580112,
+                  longitude: 126.977166,
+                }}
+              />
+            </MapView>
+          )}
+          <View style={styles.wrapper} >
+            <Text style={styles.imageText}>사진</Text>
+            <Pressable style={styles.optionButton}>
+              <Text style={styles.optionText}>선택</Text>
+            </Pressable>
+          </View>
+          <View style={styles.albumContainer}>
+            {groupedData.map((row, rowIndex) => (
+              <View key={rowIndex} style={styles.photoRow}>
+                {row.map((item, index) => (
+                  <SinglePhotoIcon key={index} />
+                ))}
+              </View>
+            ))}
+          </View>
+        </ScrollView>
       </View>
     );
   }
