c0860d813da0aa32e28dd602cc4397ce7bf994f6
g2Min <jimin9320@gmail.com>
2023-11-06 01:02:49 +0900

    feat: 개별 써클룸에 map 추가

---
+++ components/circle/single/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  mapContainer: {
+    width: "100%",
+    height: 221,
+  },
+})
--- screens/MyPage/Circle/SingleCircle.jsx
+++ screens/MyPage/Circle/SingleCircle.jsx
@@ -1,21 +1,32 @@
-import { View, Text } from 'react-native';
-import { useEffect, useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { useState, FlatList } from 'react';
 import { SplashUI } from './SplashUI';
 import { splashState } from '../../../stores/splash-store';
+import { styles } from './styles';
+import { SingleMap } from '../../../components/circle/single/SingleMap';
 
 export const SingleCircle = ({ route }) => {
   const [ isReady, setIsReady ] = useState(splashState);
+  //써클의 id값 찾아내기
   const { itemId } = route.params;
 
   if(!isReady) {
     return <SplashUI />
-  } else{
-      return(
-        <View>
-          <Text>
-            {itemId}룸입니다.
-          </Text>
+  } 
+  else{
+    return(
+      <View style={{flex:1, flexDirection: 'column', backgroundColor: '#fff'}}>
+        <View style={styles.personBox} />
+        <SingleMap />
+        <View style={styles.wrapper} >
+          <Text style={styles.imageText}>사진</Text>
+          <Pressable style={styles.optionButton}>
+            <Text style={styles.optionText}>선택</Text>
+          </Pressable>
         </View>
-      );
+      </View>
+    );
   }
 };
+
+
