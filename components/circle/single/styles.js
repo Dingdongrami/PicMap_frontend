e1e243cd524fc974c2139567d12b0c70d204b76b
g2Min <jimin9320@gmail.com>
2023-11-08 04:13:21 +0900

    fix: singlemap 컴포넌트가 singlecircle에 렌더링되지 않던 부분수정

--- components/circle/single/styles.js
+++ components/circle/single/styles.js
@@ -5,25 +5,12 @@ const { width, height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: 20,
+    height: 20,
   },
   mapContainer: {
-    flex: 1,
-    width: width,
-    height: height,
-    zIndex: 2,
-    backgroundColor: "transparent",
+    width: "100%",
+    height: "100%",
+
   },
 })
-
-    // flexDirection: 'row',
-    // width: "100%",
-    // position: 'absolute',
-    // width: "100%",
-    // minHeight: 220,
-    // top: 0,
-    // left: 0,
-    // right: 0,
-    // bottom: 0,
-    // borderWidth: 1, 
-    // marginTop: 8
