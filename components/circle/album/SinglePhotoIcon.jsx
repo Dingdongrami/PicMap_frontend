1de5c23e432abbea5283e5c4fd6387489b3921cf
g2Min <jimin9320@gmail.com>
2023-11-12 21:49:53 +0900

    feat: 버튼클릭시 앨범 및 카메라 선택

--- components/circle/album/SinglePhotoIcon.jsx
+++ components/circle/album/SinglePhotoIcon.jsx
@@ -1,24 +1,30 @@
 import { Pressable, Text, View, Image } from 'react-native';
 import { useState } from 'react';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 import Checkbox from 'expo-checkbox';
 
 export const SinglePhotoIcon = ({photoData, isSelected}) => {
   const [ checkedPhotos, setCheckedPhotos ] = useState([]);
+  const navigation = useNavigation();
+  const clickPhoto = (index) => {
+    navigation.navigate('PhotoCom', {index});
+  }
   return(
     <View>
       {photoData.map((row, rowIndex) => (
         <View key={rowIndex} style={styles.photoRow}>
           {row.map((item, index) => (
-            <Pressable key={index} >
+            <View  key={index}>
               {!isSelected ?
-                <View style={styles.imageContainer}>
-                  <Image source={require('../../../assets/icons/image.png')} style={styles.imageIcon} />
-                </View>
+                <Pressable onPress={()=>clickPhoto(3*rowIndex + index)} >
+                  <View style={styles.imageContainer} >
+                    <Image source={require('../../../assets/icons/image.png')} style={styles.imageIcon} />
+                  </View>
+                </Pressable>
                 : 
                 <View style={styles.imageCon4check}>
                   <Checkbox 
-                    key={3*rowIndex+index}
                     value={checkedPhotos[3*rowIndex+index]}
                     onValueChange={() => {
                       const itemIndex = 3*rowIndex+index;
@@ -32,7 +38,7 @@ export const SinglePhotoIcon = ({photoData, isSelected}) => {
                   <Image source={require('../../../assets/icons/image.png')} style={styles.image4check} />
                 </View>
               }
-            </Pressable>
+            </View>
           ))}
         </View>
       ))}
