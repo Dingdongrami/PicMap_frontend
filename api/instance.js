c8db5b84fc54bc213f42161eaeac5ea6d6e62493
Kong-E <kng001016@dgu.ac.kr>
2023-11-13 12:29:44 +0900

    feat: api instance 생성

---
+++ api/instance.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+// 인증 토큰을 가져오는 함수 (여기서는 예시로 localStorage를 사용)
+const getAuthToken = () => localStorage.getItem('token');
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  timeout: 1000,
+});
+
+// 요청 인터셉터를 추가하여 요청이 전송되기 전에 실행됩니다.
+instance.interceptors.request.use(
+  config => {
+    // 토큰을 가져옵니다.
+    const token = getAuthToken();
+
+    // 토큰이 있으면 Authorization 헤더를 설정합니다.
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  error => {
+    // 요청 오류가 있는 경우 여기서 처리합니다.
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
