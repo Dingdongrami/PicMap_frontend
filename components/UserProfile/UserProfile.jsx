2da80f33df6922ee8923bf1adf02770f73023ba7
Kong-E <kng001016@dgu.ac.kr>
2023-11-13 11:47:57 +0900

    feat: 친구 요청 토스트 구현

--- components/UserProfile/UserProfile.jsx
+++ components/UserProfile/UserProfile.jsx
@@ -3,8 +3,21 @@ import { styles } from '../MyProfile/styles';
 import { useRecoilState } from 'recoil';
 import { userState } from '../../stores/user-store';
 import { Image } from 'expo-image';
+import { useState } from 'react';
+import CustomToast from '../CustomToast';
 
 const UserProfile = ({ user, onPressFriendRequest }) => {
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+
+  const onPressRequest = () => {
+    // TODO: 친구요청 로직을 여기에 추가하세요.
+
+    // 친구요청 로직이 성공했다고 가정하고 토스트 메시지를 띄웁니다.
+    setToastMessage('친구 요청 완료');
+    setShowToast(true);
+  };
+
   return (
     <View style={styles.profileContainer}>
       {user.profileImage ? (
@@ -21,12 +34,13 @@ const UserProfile = ({ user, onPressFriendRequest }) => {
         </View>
         <Text style={styles.onelineText}>{user.introduction}</Text>
         <View style={styles.buttonWrapper}>
-          <Pressable style={styles.pinkButton} onPress={onPressFriendRequest}>
+          <Pressable style={styles.pinkButton} onPress={onPressRequest}>
             <Image source={require('../../assets/icons/person_add.png')} style={styles.friendsImage} />
-            <Text style={styles.buttonText}>친구요청</Text>
+            <Text style={styles.buttonText}>{showToast ? '친구 요청 중' : '친구 요청'}</Text>
           </Pressable>
         </View>
       </View>
+      <CustomToast text={toastMessage} show={showToast} />
     </View>
   );
 };
