0e16a28a8252db21dfc93a845bce045fa4d5a40b
g2Min <jimin9320@gmail.com>
2023-11-20 00:55:38 +0900

    feat: '선택'옵션 recoil로 전역상태변수

--- components/circle/album/styles.js
+++ components/circle/album/styles.js
@@ -6,7 +6,6 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 4,
-    paddingHorizontal: 3,
   },
   imageContainer: {
     flex: 1,
@@ -17,8 +16,8 @@ export const styles = StyleSheet.create({
     borderColor: '#44403C',
     justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: 2,
-    marginVertical: 2,
+    // marginHorizontal: 2,
+    // marginVertical: 2,
   },
   imageIcon: {
     width: 45,
@@ -33,6 +32,12 @@ export const styles = StyleSheet.create({
     borderColor: '#44403C',
     marginHorizontal: 2,
     marginVertical: 2,
+  },
+  image4check: {
+    width: 45,
+    height: 31,
+    marginLeft: 37,
+    marginTop: 21,
   },
   photoRow: {
     flexDirection: 'row',
