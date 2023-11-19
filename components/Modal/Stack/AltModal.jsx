9815155a1477a3f1baf889e5b868cb1d78ce3893
g2Min <jimin9320@gmail.com>
2023-11-19 21:26:55 +0900

    feat: screenOption이 동일한 stack들 group으로 묶기

---
+++ components/Modal/Stack/AltModal.jsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { BottomModal } from "../Modal";
+import { NestedModal } from "../NestedModal";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
+
+export const AltModal = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group screenOptions={{ presentation: 'modal'}}>
+          <Stack.Screen name="BottomModal" component={BottomModal} />
+          <Stack.Screen name="NestedModal" component={NestedModal} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
