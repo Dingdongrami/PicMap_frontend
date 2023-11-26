fbfae3251840131d3412291a1fe6ba0b6caa9ea7
g2Min <jimin9320@gmail.com>
2023-11-26 13:29:58 +0900

    test: google map marker 실험

--- screens/MyPage/Map/Map.jsx
+++ screens/MyPage/Map/Map.jsx
@@ -4,77 +4,110 @@ import MapView from 'react-native-maps';
 import { styles } from './styles';
 import { useRef } from 'react';
 
+
 export const Map = () => {
   const mapRef = useRef(null);
   const goToMonument = (monument) => {
     mapRef.current.animateToRegion(monument, 3*250);
   };
+  const INIT = {
+    latitude: 37.580112,
+    longitude: 126.977166,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
   const examples = [
     {
-      default: {
-        latitude: 41.809745,
-        longitude: 2.097774,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-    },
-    {
-      ex1: {
-        latitude: 41.811501,
-        longitude: 2.098318,
+      // ex1: {
+      //   latitude: 37.00,
+      //   longitude: 126.90,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01,
+      //   source: require('../../../assets/example/ex1.png')
+      // },
+      id: 1,
+      coordinate: {
+        latitude: 37.00,
+        longitude: 126.90,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
-        source: require('../../../assets/example/ex1.png')
       },
+      source: require('../../../assets/example/ex1.png')
     },
     {
-      ex2: {
-        latitude: 41.811991,
-        longitude: 2.098610,
+      // ex2: {
+        // latitude: 32,
+        // longitude: 126.66,
+        // latitudeDelta: 0.01,
+        // longitudeDelta: 0.01,
+      //   source: require('../../../assets/example/ex2.png')
+      // }
+      id:1,
+      coordinate: {
+        latitude: 32,
+        longitude: 126.66,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,        
+      },
+      source: require('../../../assets/example/ex2.png')
+    },
+    {
+      // ex3: {
+        // latitude: 31.12,
+        // longitude: 126.977166,
+        // latitudeDelta: 0.01,
+        // longitudeDelta: 0.01,
+      //   source: require('../../../assets/example/ex3.png')
+      // },
+      id: 3,
+      coordinate: {
+        latitude: 31.12,
+        longitude: 126.977166,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
-        source: require('../../../assets/example/ex2.png')
-
-      }
-    },
-    {
-      ex3: {
-        latitude: 41.812629,
-        longitude: 2.094989,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-        source: require('../../../assets/example/ex3.png')
       },
+      source: require('../../../assets/example/ex3.png') 
     },
     {
-      ex4: {
+      // ex4: {
+      //   latitude: 41.812246,
+      //   longitude: 2.087440,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01,
+      //   source: require('../../../assets/example/ex4.png')
+      // },
+      id: 4,
+      coordinate: {
         latitude: 41.812246,
         longitude: 2.087440,
         latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-        source: require('../../../assets/example/ex4.png')
+        longitudeDelta: 0.01,        
       },
+      source:  require('../../../assets/example/ex4.png')
     },
     {
-      ex5: {
+      // ex5: {
+        // latitude: 41.813073,
+        // longitude: 2.097226,
+        // latitudeDelta: 0.01,
+        // longitudeDelta: 0.01,
+      //   source: require('../../../assets/example/ex5.png')
+      // }
+      id: 5,
+      coordinate: {
         latitude: 41.813073,
         longitude: 2.097226,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
-        source: require('../../../assets/example/ex5.png')
-      }
+      },
+      source: require('../../../assets/example/ex5.png')
     }
   ];
   return (
     <MapView
       style={styles.map}
-      // initialRegion={{
-      //   latitude: 37.580112,
-      //   longitude: 126.977166,
-      //   latitudeDelta: 0.01,
-      //   longitudeDelta: 0.01,
-      // }}
-      initialRegion={examples.default}
+      initialRegion={INIT}
+      // initialRegion={examples.default}
       provider={PROVIDER_GOOGLE}>
       {/* <Marker
         coordinate={{
@@ -86,7 +119,7 @@ export const Map = () => {
         description="테스트"
       /> */}
       {examples.map((item, index) => (
-        <Marker coordinate={item} imageSource={item.source} />
+        <Marker key={index} coordinate={item} imageSource={item.source} />
       ))}
     </MapView>
   );
