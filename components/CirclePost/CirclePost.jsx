3c3d9d401076e89edf3972627136b2f5bd88f320
Kong-E <kng001016@dgu.ac.kr>
2023-11-29 12:09:37 +0900

    refactor: circlePost, Photos fetch using useQuery

--- components/CirclePost/CirclePost.jsx
+++ components/CirclePost/CirclePost.jsx
@@ -1,37 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { getPhotoBorderStyle } from '../../utils/getPhotoBorderStyles';
 import { Image } from 'expo-image';
 import { s3BaseUrl } from '../../constants/config';
-import { useEffect, useState } from 'react';
-import { photoInstance } from '../../api/instance';
+import { useQuery } from '@tanstack/react-query';
+import { fetchPhotos } from '../../api/circleApi';
 
 const CirclePost = ({ item }) => {
-  const [photos, setPhotos] = useState([]);
   const circleId = item.id;
 
-  const getPhotos = async () => {
-    //써클의 사진들을 가져오는 함수
-    const response = await photoInstance.get(`get/circle/${circleId}`);
-    console.log(response.data);
-    setPhotos(response.data);
-  };
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['circlePost', circleId],
+    queryFn: () => fetchPhotos(circleId),
+  });
 
-  useEffect(() => {
-    getPhotos();
-  }, []);
+  if (isLoading) {
+    return <Text>Loading...</Text>; // Or any other loading indicator
+  }
 
-  // 조건부 렌더링 수정
+  if (isError) {
+    return <Text>Error fetching data</Text>; // Or any error indicator
+  }
+
   return (
-    photos.length > 0 && (
+    data?.length > 3 && (
       <>
         <View style={styles.circle}>
-          {photos.slice(0, 4).map((photo, index) => (
+          {data.slice(0, 4).map((photo, index) => (
             <View key={index} style={styles.photoWrapper}>
               <Image
-                style={[styles.photo, , getPhotoBorderStyle(index)]}
-                source={s3BaseUrl + photo.filePath} // Image 컴포넌트의 source prop 수정
-                contentFit="cover" // contentFit 대신 resizeMode 사용
+                style={[styles.photo, getPhotoBorderStyle(index)]}
+                source={{ uri: s3BaseUrl + photo.filePath }}
+                resizeMode="cover"
               />
             </View>
           ))}
