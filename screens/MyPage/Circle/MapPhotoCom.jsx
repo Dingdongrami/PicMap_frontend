d6c84920969d07b0f578c4a8ecc50d351230d811
g2Min <jimin9320@gmail.com>
2023-12-08 07:29:53 +0900

    feat: 써클앨범끼리 사진 swipe 애니메이션

--- screens/MyPage/Circle/MapPhotoCom.jsx
+++ screens/MyPage/Circle/MapPhotoCom.jsx
@@ -1,4 +1,4 @@
-import { View, Image, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { comStyles, PhotoComments } from '../../../components/circle';
 import { s3BaseUrl } from '../../../constants/config';
